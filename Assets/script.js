93ac69b42c6960e8850a22fd8b2eec2caf9c48cf
MSchuur <mschuur31@gmail.com>
2023-02-02 12:19:23 -0400

    Changed the number of max characters to 128 from 129 IAW the Acceptance Criteria

--- Assets/script.js
+++ Assets/script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 var passWordLower = ['abcdefghijklmnopqrstuvwxyz'];
 var passWordUpper = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 var passWordNum = ['0123456789'];
-var passWordSpecial = ['!@#$%^&*'];
+var passWordSpecial = ['!@#$%^&*"()+_-=/?.>,<[]\|{}~`'];
 var passWordArray = [];
 var passWordLength;
 var passWordIndex = ["Lower Case Letters", "Upper Case Letters", "Numbers", "Special Characters"]
@@ -18,9 +18,9 @@ function writePassword() {
 
   // Prompt to select the number of characters for the password
   function wordLength() {
-    passWordLength = window.prompt("How many charatcers long do you wish to make your new Password (8-129)");
+    passWordLength = window.prompt("How many charatcers long do you wish to make your new Password (8-128)");
     if (passWordLength <=7 || passWordLength >128) {
-      window.alert("The number of characters that you have chosen does not meet the criteria. Please choose a number between 8 and 129!");
+      window.alert("The number of characters that you have chosen does not meet the criteria. Please choose a number between 8 and 128!");
       wordLength();
     }
     return
