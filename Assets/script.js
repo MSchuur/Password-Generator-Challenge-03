b388462128ace67bf479b9ddd3f304dbc089249a
MSchuur <mschuur31@gmail.com>
2023-02-02 21:15:01 -0400

    Made the JS layout and order of functioning easier to follow

--- Assets/script.js
+++ Assets/script.js
@@ -10,62 +10,59 @@ var passWordArray = [];
 var passWordLength;
 var passWordIndex = ["Lower Case Letters", "Upper Case Letters", "Numbers", "Special Characters"]
 var passWordVar = [passWordLower, passWordUpper, passWordNum, passWordSpecial];
+var passwordText = document.querySelector("#password");
 
+// Function to create the length of the password
+function wordLength() {
+  passWordLength = window.prompt("How many charatcers long do you wish to make your new Password (8-128)");
+  if (passWordLength === null) {
+    return;
+  }  
+  else if (passWordLength <=7 || passWordLength >128) {
+    window.confirm("The number of characters that you have chosen does not meet the criteria. Please choose a number between 8 and 128!");
+    wordLength();
+  }
+  return
+}
 
+//  Function to creating an Array of all eligable characters from the prompts
+function generteCharacters () {
+  for (i=0; i < passWordIndex.length; i++) {
+    var answer = window.prompt("Do you want to use " + passWordIndex[i] + " in your Password? Enter Y or N");
+      if(answer === null) {
+        return;
+      }
+    answer = answer.toUpperCase();
+      if (answer === "Y") {
+        passWordArray = passWordArray + passWordVar[i];
+        console.log(passWordArray);
+      }
+      else {
+        answer = window.alert("Your Password is not as strong as it can be if you do not use " +  passWordIndex[i]);
+      }
+  }
+}
+
+// Function to generate the random password
+function generatePassword(passwordText) {
+  passwordText = "";
+  for (i=0; i < passWordLength; i ++) {
+    passwordText = passwordText + passWordArray[(Math.floor(Math.random() * passWordArray.length))];
+  }
+  console.log(passwordText)
+  return passwordText
+}
 
 // Write password to the #password input
 function writePassword() {
   passWordLength = 0;
   passWordArray = "";
-    
-  // Function to create the length of the password
-  function wordLength() {
-    passWordLength = window.prompt("How many charatcers long do you wish to make your new Password (8-128)");
-    if (passWordLength === null) {
-      return;
-    }  
-    else if (passWordLength <=7 || passWordLength >128) {
-      window.alert("The number of characters that you have chosen does not meet the criteria. Please choose a number between 8 and 128!");
-      wordLength();
-    }
-    return
-  }
   // Calls function to detirmine the length of the password
   wordLength();
-    
-  //  Function to creating an Array of all eligable characters from the prompts
-  function generteCharacters () {
-    for (i=0; i < passWordIndex.length; i++) {
-      var answer = window.prompt("Do you want to use " + passWordIndex[i] + " in your Password? Enter Y or N");
-        if(answer === null) {
-          return;
-        }
-      answer = answer.toUpperCase();
-        if (answer === "Y") {
-          passWordArray = passWordArray + passWordVar[i];
-          console.log(passWordArray);
-        }
-        else {
-          answer = window.alert("Your Password is not as strong as it can be if you do not use " +  passWordIndex[i]);
-        }
-    }
-  }
-
   // Call function to generates the characters if there is a correct password length
   if (passWordLength>7) {
     generteCharacters();
   }
-  
-  // Function to generate the random password
-  function generatePassword(passwordText) {
-    passwordText = "";
-    for (i=0; i < passWordLength; i ++) {
-      passwordText = passwordText + passWordArray[(Math.floor(Math.random() * passWordArray.length))];
-    }
-    console.log(passwordText)
-    return passwordText
-  }
-
   // Call function to generates password if a character set was been selceted.
   if (passWordArray.length >0) {
     var password = generatePassword();
@@ -73,7 +70,7 @@ function writePassword() {
   else {
     return
   }
-  var passwordText = document.querySelector("#password");
+  passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
